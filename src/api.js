fde246ecf60f1681212663e6d2b78528ea94064f
Comondear <elcomondear@gmail.com>
2022-12-13 14:00:23 -0700

    fixed-api

--- src/api.js
+++ src/api.js
@@ -1,15 +1,24 @@
 import axios from 'axios';
-axios.defaults.baseURL = 'https://pixabay.com/api/';
-const params = {
-  key: '30602892-abd71b62e0861d27e9591816d',
+
+const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = '28916923-d414eade46dc2c6542ccb3cb8';
+const IMAGES_PER_PAGE = 12;
+
+axios.defaults.baseURL = BASE_URL;
+axios.defaults.params = {
+  key: API_KEY,
   image_type: 'photo',
   orientation: 'horizontal',
-  safesearch: 'true',
-  per_page: 12,
-  isImageModalOpen: false,
+  per_page: IMAGES_PER_PAGE,
 };
 
 export const fetchImages = async (query, page) => {
-  const response = await axios.get(`/?q=${query}&page=${page}`, { params });
+  const config = {
+    params: {
+      q: query,
+      page: page,
+    },
+  };
+  const response = await axios.get('', config);
   return response.data;
 };
